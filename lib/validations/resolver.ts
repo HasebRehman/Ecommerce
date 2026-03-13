8703c282a60b6f61769d081dd0399f2d8b928f4c
HasebRehman <haseebrehman3460@gmail.com>
2026-03-13 14:08:14 +0500

    handle errors to deploy

--- lib/validations/resolver.ts
+++ lib/validations/resolver.ts
@@ -2,5 +2,5 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import type { ZodType, ZodTypeDef } from 'zod'
 
 export function createResolver<T>(schema: ZodType<T, ZodTypeDef, T>) {
-  return zodResolver(schema) as any
+  return zodResolver(schema as any) 
 }
